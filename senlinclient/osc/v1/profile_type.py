827d5c84ae5898c68fcd7ecb63a0b59aba5b6244
dixiaoli <dixiaobj@cn.ibm.com>
2016-02-19 09:59:24 +0800

    Add OpenstackClient plugin for cluster profile type show
    This change implements the "openstack cluster profile type show" command
      Based on the existing senlin command: senlin profile-type-show
    
    Change-Id: Iab715ce2f2f13432e60187f42ca34d07c1f29fd0
    Blueprint: senlin-support-python-openstackclient

--- senlinclient/osc/v1/profile_type.py
+++ senlinclient/osc/v1/profile_type.py
@@ -13,8 +13,13 @@
 """Clustering v1 profile type action implementations"""
 
 import logging
+import six
 
 from cliff import lister
+from openstack import exceptions as sdk_exc
+from openstackclient.common import exceptions as exc
+from senlinclient.common import format_utils
+from senlinclient.common.i18n import _
 
 
 class ProfileTypeList(lister.Lister):
@@ -32,4 +37,32 @@ class ProfileTypeList(lister.Lister):
         types = senlin_client.profile_types()
         columns = ['name']
         rows = sorted([t.name] for t in types)
+        return columns, rows
+
+
+class ProfileTypeShow(format_utils.YamlFormat):
+    """Show the details about a profile type."""
+
+    log = logging.getLogger(__name__ + ".ProfileTypeShow")
+
+    def get_parser(self, prog_name):
+        parser = super(ProfileTypeShow, self).get_parser(prog_name)
+        parser.add_argument(
+            'type_name',
+            metavar='<TYPE_NAME>',
+            help=_('Profile type to retrieve')
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+        senlin_client = self.app.client_manager.clustering
+
+        try:
+            res = senlin_client.get_profile_type(parsed_args.type_name)
+        except sdk_exc.ResourceNotFound:
+            raise exc.CommandError(_('Profile Type not found: %s')
+                                   % parsed_args.type_name)
+        rows = list(six.itervalues(res))
+        columns = list(six.iterkeys(res))
         return columns, rows
