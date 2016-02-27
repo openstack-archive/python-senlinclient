e3d0cc06c317fa06f00490317d9cb8216c73ef03
dixiaoli <dixiaobj@cn.ibm.com>
2016-02-28 02:43:39 +0800

    Add OSC plugin for openstack cluster build info
    This change implements the "openstack cluster build info" command
      Based on the existing senlin command: senlin build-info
    
    Change-Id: I18f5f79c15b7ebbdfe00c4d88c2b687a742d2056
    Blueprint: senlin-support-python-openstackclient

---
+++ senlinclient/tests/unit/osc/v1/test_build_info.py
@@ -0,0 +1,42 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+
+from openstack.cluster.v1 import build_info as sdk_build_info
+
+from senlinclient.osc.v1 import build_info as osc_build_info
+from senlinclient.tests.unit.osc.v1 import fakes
+
+
+class TestBuildInfo(fakes.TestClusteringv1):
+    response = {"build_info": {
+        "api": {
+            "revision": "1.0"
+        },
+        "engine": {
+            "revision": "1.0"
+        }
+    }}
+
+    def setUp(self):
+        super(TestBuildInfo, self).setUp()
+        self.cmd = osc_build_info.BuildInfo(self.app, None)
+        self.mock_client = self.app.client_manager.clustering
+        self.mock_client.get_build_info = mock.Mock(
+            return_value=sdk_build_info.BuildInfo(None, self.response))
+
+    def test_build_info(self):
+        arglist = []
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+        self.cmd.take_action(parsed_args)
+        self.mock_client.get_build_info.assert_called_with()
