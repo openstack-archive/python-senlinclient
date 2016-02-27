343f4449ee1fab1b4c4a4031d143e32123552dc1
dixiaoli <dixiaobj@cn.ibm.com>
2016-02-28 00:51:23 +0800

    Add OpenstackClient plugin for cluster members list
    This change implements the "openstack cluster members list" command
      Based on the existing senlin command: senlin cluster-node-list
    
    Change-Id: I97c720705a0b46611b855b6fb800ef667d070333
    Blueprint: senlin-support-python-openstackclient

--- senlinclient/osc/v1/cluster.py
+++ senlinclient/osc/v1/cluster.py
@@ -584,3 +584,78 @@ class ClusterPolicyDetach(command.Command):
         resp = senlin_client.cluster_detach_policy(parsed_args.cluster,
                                                    parsed_args.policy)
         print('Request accepted by action: %s' % resp['action'])
+
+
+class ClusterNodeList(lister.Lister):
+    """List nodes from cluster."""
+
+    log = logging.getLogger(__name__ + ".ClusterNodeList")
+
+    def get_parser(self, prog_name):
+        parser = super(ClusterNodeList, self).get_parser(prog_name)
+        parser.add_argument(
+            '--filters',
+            metavar='<key1=value;key2=value2...>',
+            help=_('Filter parameters to apply on returned nodes. '
+                   'This can be specified multiple times, or once with '
+                   'parameters separated by a semicolon'),
+            action='append'
+        )
+        parser.add_argument(
+            '--sort',
+            metavar='<key:direction>',
+            help=_('Sorting option which is a string containing a list of '
+                   'keys separated by commas. Each key can be optionally '
+                   'appended by a sort direction (:asc or :desc)')
+        )
+        parser.add_argument(
+            '--limit',
+            metavar='<limit>',
+            help=_('Limit the number of nodes returned')
+        )
+        parser.add_argument(
+            '--marker',
+            metavar='<id>',
+            help=_('Only return nodes that appear after the given node ID')
+        )
+        parser.add_argument(
+            '--full-id',
+            default=False,
+            action="store_true",
+            help=_('Print full IDs in list')
+        )
+        parser.add_argument(
+            'cluster',
+            metavar='<cluster>',
+            help=_('Name or ID of cluster to nodes from')
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+        senlin_client = self.app.client_manager.clustering
+        queries = {
+            'cluster_id': parsed_args.cluster,
+            'sort': parsed_args.sort,
+            'limit': parsed_args.limit,
+            'marker': parsed_args.marker,
+        }
+        if parsed_args.filters:
+            queries.update(senlin_utils.format_parameters(parsed_args.filters))
+
+        nodes = senlin_client.nodes(**queries)
+        if not parsed_args.full_id:
+            formatters = {
+                'id': lambda x: x[:8],
+                'physical_id': lambda x: x[:8] if x else ''
+            }
+        else:
+            formatters = {}
+
+        columns = ['id', 'name', 'index', 'status', 'physical_id',
+                   'created_at']
+        return (
+            columns,
+            (utils.get_item_properties(n, columns, formatters=formatters)
+             for n in nodes)
+        )
