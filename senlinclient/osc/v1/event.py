3fbdd4fa691e4e8104f92e204066017585fe30f6
dixiaoli <dixiaobj@cn.ibm.com>
2016-02-27 20:16:58 +0800

    Add OSC plugin for openstack cluster event list
    This change implements the "openstack cluster event list" command
      Based on the existing senlin command: senlin event-list
    
    Change-Id: Ibcdb25978c551226d3600c80ee7a557574e338e0
    Blueprint: senlin-support-python-openstackclient

---
+++ senlinclient/osc/v1/event.py
@@ -0,0 +1,102 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Clustering v1 event action implementations"""
+
+import logging
+
+from cliff import lister
+from openstackclient.common import utils
+
+from senlinclient.common.i18n import _
+from senlinclient.common import utils as senlin_utils
+
+
+class ListEvent(lister.Lister):
+    """List events."""
+
+    log = logging.getLogger(__name__ + ".ListEvent")
+
+    def get_parser(self, prog_name):
+        parser = super(ListEvent, self).get_parser(prog_name)
+        parser.add_argument(
+            '--filters',
+            metavar='<key1=value1;key2=value2...>',
+            help=_("Filter parameters to apply on returned events. "
+                   "This can be specified multiple times, or once with "
+                   "parameters separated by a semicolon. The valid filter "
+                   "keys are: ['level', 'obj_type', 'obj_id' ,'cluster_id', "
+                   "'obj_name', 'action']"),
+            action='append'
+        )
+        parser.add_argument(
+            '--limit',
+            metavar='<limit>',
+            help=_('Limit the number of events returned')
+        )
+        parser.add_argument(
+            '--marker',
+            metavar='<id>',
+            help=_('Only return events that appear after the given event ID')
+        )
+        parser.add_argument(
+            '--sort',
+            metavar='<key:dir>',
+            help=_("Sorting option which is a string containing a list of "
+                   "keys separated by commas. Each key can be optionally "
+                   "appended by a sort direction (:asc or :desc). The valid "
+                   "sort keys are: ['timestamp', 'level', 'obj_type', "
+                   "'obj_name', 'user', 'action', 'status']")
+        )
+        parser.add_argument(
+            '--global-project',
+            default=False,
+            action="store_true",
+            help=_('Whether events from all projects should be listed. '
+                   ' Default to False. Setting this to True may demand '
+                   'for an admin privilege')
+        )
+        parser.add_argument(
+            '--full-id',
+            default=False,
+            action="store_true",
+            help=_('Print full IDs in list')
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+
+        senlin_client = self.app.client_manager.clustering
+        columns = ['id', 'timestamp', 'obj_type', 'obj_id', 'obj_name',
+                   'action', 'status', 'status_reason', 'level']
+        queries = {
+            'sort': parsed_args.sort,
+            'limit': parsed_args.limit,
+            'marker': parsed_args.marker,
+            'global_project': parsed_args.global_project,
+        }
+
+        if parsed_args.filters:
+            queries.update(senlin_utils.format_parameters(parsed_args.filters))
+
+        formatters = {}
+        if not parsed_args.full_id:
+            formatters['id'] = lambda x: x[:8]
+            formatters['obj_id'] = lambda x: x[:8] if x else ''
+
+        events = senlin_client.events(**queries)
+        return (
+            columns,
+            (utils.get_item_properties(e, columns, formatters=formatters)
+             for e in events)
+        )
